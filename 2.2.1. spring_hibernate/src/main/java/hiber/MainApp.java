da61c0a8797b7980d785bc8df320cb57305d8cf0
Andrey Belov <belovandr96@gmail.com>
2023-05-03 22:43:47 +0500

    complete task

--- 2.2.1. spring_hibernate/src/main/java/hiber/MainApp.java
+++ 2.2.1. spring_hibernate/src/main/java/hiber/MainApp.java
@@ -1,6 +1,7 @@
 package hiber;
 
 import hiber.config.AppConfig;
+import hiber.model.Car;
 import hiber.model.User;
 import hiber.service.UserService;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
@@ -15,10 +16,17 @@ public class MainApp {
 
       UserService userService = context.getBean(UserService.class);
 
-      userService.add(new User("User1", "Lastname1", "user1@mail.ru"));
-      userService.add(new User("User2", "Lastname2", "user2@mail.ru"));
-      userService.add(new User("User3", "Lastname3", "user3@mail.ru"));
-      userService.add(new User("User4", "Lastname4", "user4@mail.ru"));
+      Car car = new Car("Volvo1", 2281);
+
+      User user1 = new User("User1", "Lastname1", "user1@mail.ru", car);
+      User user2 = new User("User2", "Lastname2", "user2@mail.ru", new Car("Volvo2", 2282));
+      User user3 = new User("User3", "Lastname3", "user3@mail.ru", new Car("Volvo3", 2283));
+      User user4 = new User("User4", "Lastname4", "user4@mail.ru", new Car("Volvo4", 2284));
+
+      userService.add(user1);
+      userService.add(user2);
+      userService.add(user3);
+      userService.add(user4);
 
       List<User> users = userService.listUsers();
       for (User user : users) {
@@ -26,8 +34,10 @@ public class MainApp {
          System.out.println("First Name = "+user.getFirstName());
          System.out.println("Last Name = "+user.getLastName());
          System.out.println("Email = "+user.getEmail());
-         System.out.println();
+         System.out.println("Car = " +user.getCar());
       }
+
+      System.out.println(userService.getUserByCarModelAndSeries("Volvo3", 2283));
 
       context.close();
    }
--- 2.2.1. spring_hibernate/src/main/java/hiber/dao/UserDaoImp.java
+++ 2.2.1. spring_hibernate/src/main/java/hiber/dao/UserDaoImp.java
@@ -1,29 +1,48 @@
 package hiber.dao;
 
+import hiber.model.Car;
 import hiber.model.User;
 import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
-import javax.persistence.TypedQuery;
+import javax.persistence.*;
 import java.util.List;
 
 @Repository
 public class UserDaoImp implements UserDao {
 
-   @Autowired
-   private SessionFactory sessionFactory;
+    private final static String HQL_GET_USER_BY_CAR = "FROM User as user " +
+            "INNER JOIN FETCH user.car as userCar " +
+            "WHERE userCar.model=:carModel AND userCar.series=:carSeries";
 
-   @Override
-   public void add(User user) {
-      sessionFactory.getCurrentSession().save(user);
-   }
+    @Autowired
+    private SessionFactory sessionFactory;
 
-   @Override
-   @SuppressWarnings("unchecked")
-   public List<User> listUsers() {
-      TypedQuery<User> query=sessionFactory.getCurrentSession().createQuery("from User");
-      return query.getResultList();
-   }
+    @Override
+    public void add(User user) {
+        sessionFactory.getCurrentSession().save(user);
+    }
 
+    @Override
+    @SuppressWarnings("unchecked")
+    public List<User> listUsers() {
+        TypedQuery<User> query = sessionFactory.getCurrentSession().createQuery("from User");
+        return query.getResultList();
+    }
+
+    @Override
+    public User getUserByCarModelAndSeries(String model, int series) {
+        try {
+            TypedQuery<User> result = sessionFactory.getCurrentSession().createQuery(HQL_GET_USER_BY_CAR)
+                    .setParameter("carModel", model)
+                    .setParameter("carSeries", series);
+            return result.getSingleResult();
+        } catch (NoResultException e) {
+            System.out.println("Пользователя с указанной моделью и серией машины найдено не было");
+        } catch (NonUniqueResultException e) {
+            System.out.println("Было найдено несколько пользователей с указанной моделью и серией машины");
+        }
+        return null;
+    }
 }
