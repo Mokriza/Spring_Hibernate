f86bfc82694d6149ed6cce41c3e0823ecd30f3ee
Andrey Belov <belovandr96@gmail.com>
2023-05-04 18:15:19 +0500

    add addCar and getCarsList methods

--- 2.2.1. spring_hibernate/src/main/java/hiber/service/CarService.java
+++ 2.2.1. spring_hibernate/src/main/java/hiber/service/CarService.java
@@ -1,4 +1,13 @@
 package hiber.service;
 
+import hiber.model.Car;
+import hiber.model.User;
+
+import java.util.List;
+
 public interface CarService {
+
+    void addCar(Car car);
+    List<Car> getCarsList();
+
 }
