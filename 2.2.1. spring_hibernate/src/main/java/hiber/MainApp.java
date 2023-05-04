5d50b019332506aafddda4fc8b25661430591980
Andrey Belov <belovandr96@gmail.com>
2023-05-04 18:14:49 +0500

    add tests for CarDAO methods

--- 2.2.1. spring_hibernate/src/main/java/hiber/MainApp.java
+++ 2.2.1. spring_hibernate/src/main/java/hiber/MainApp.java
@@ -3,6 +3,7 @@ package hiber;
 import hiber.config.AppConfig;
 import hiber.model.Car;
 import hiber.model.User;
+import hiber.service.CarService;
 import hiber.service.UserService;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
@@ -10,35 +11,46 @@ import java.sql.SQLException;
 import java.util.List;
 
 public class MainApp {
-   public static void main(String[] args) throws SQLException {
-      AnnotationConfigApplicationContext context = 
-            new AnnotationConfigApplicationContext(AppConfig.class);
+    public static void main(String[] args) throws SQLException {
+        AnnotationConfigApplicationContext context =
+                new AnnotationConfigApplicationContext(AppConfig.class);
 
-      UserService userService = context.getBean(UserService.class);
+        UserService userService = context.getBean(UserService.class);
+        CarService carService = context.getBean(CarService.class);
 
-      Car car = new Car("Volvo1", 2281);
+        Car car1 = new Car("Volvo", 228);
+        Car car2 = new Car("Lada", 14932);
+        Car car3 = new Car("Mercedes", 660);
+        Car car4 = new Car("Skoda", 2315);
 
-      User user1 = new User("User1", "Lastname1", "user1@mail.ru", car);
-      User user2 = new User("User2", "Lastname2", "user2@mail.ru", new Car("Volvo2", 2282));
-      User user3 = new User("User3", "Lastname3", "user3@mail.ru", new Car("Volvo3", 2283));
-      User user4 = new User("User4", "Lastname4", "user4@mail.ru", new Car("Volvo4", 2284));
+        User user1 = new User("User1", "Lastname1", "user1@mail.ru", car1);
+        User user2 = new User("User2", "Lastname2", "user2@mail.ru", car2);
+        User user3 = new User("User3", "Lastname3", "user3@mail.ru", car3);
+        User user4 = new User("User4", "Lastname4", "user4@mail.ru", car4);
 
-      userService.addUser(user1);
-      userService.addUser(user2);
-      userService.addUser(user3);
-      userService.addUser(user4);
+        userService.addUser(user1);
+        userService.addUser(user2);
+        userService.addUser(user3);
+        userService.addUser(user4);
 
-      List<User> users = userService.getUsersList();
-      for (User user : users) {
-         System.out.println("Id = "+user.getId());
-         System.out.println("First Name = "+user.getFirstName());
-         System.out.println("Last Name = "+user.getLastName());
-         System.out.println("Email = "+user.getEmail());
-         System.out.println("Car = " +user.getCar());
-      }
+        List<User> users = userService.getUsersList();
+        for (User user : users) {
+            System.out.println("Id = " + user.getId());
+            System.out.println("First Name = " + user.getFirstName());
+            System.out.println("Last Name = " + user.getLastName());
+            System.out.println("Email = " + user.getEmail());
+            System.out.println("Car = " + user.getCar());
+        }
+        List<Car> cars = carService.getCarsList();
+        for (Car car : cars) {
+            System.out.println("Id = " + car.getId());
+            System.out.println("Model = " + car.getModel());
+            System.out.println("Series = " + car.getSeries());
+        }
 
-      System.out.println(userService.getUserByCarModelAndSeries("Volvo3", 2283));
 
-      context.close();
-   }
+        System.out.println(userService.getUserByCarModelAndSeries("Mercedes", 660));
+
+        context.close();
+    }
 }
